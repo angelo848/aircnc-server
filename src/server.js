43ce0a7ca62e7715a3556cbb78583531972ea05e
Angelo Salles <angelo_848@live.com>
2019-12-12 17:14:52 -0300

    alterado response

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    return res.json({ message: 'Hello world' })
+    return res.json({ message: 'Hello omnistack' })
 })
 
 app.listen(3333)
