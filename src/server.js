428e6029e607b9ce5a5c89b8528b12f044bdcf7a
Angelo Salles <angelo_848@live.com>
2019-12-15 15:44:20 -0300

    adaptacoes para frontend e mobile

--- src/server.js
+++ src/server.js
@@ -1,9 +1,20 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const path = require('path')
+
+const routes = require('./routes')
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-pqbnn.mongodb.net/semana09?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
 
 const app = express()
 
-app.get('/', (req, res) => {
-    return res.json({ message: 'Hello world' })
-})
+app.use(cors())
+app.use(express.json())
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')))
+app.use(routes)
 
 app.listen(3333)
